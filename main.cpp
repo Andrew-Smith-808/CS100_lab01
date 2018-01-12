35633a07c7a85d1b01604464afb034cbc1c47264
Andrew Smith <andrewsmith@ucrwpa1-6-10-25-23-38.wnet.ucr.edu>
2018-01-11 18:55:21 -0800

    added user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
-int main() {
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
 
-    std::cout << "hello git" << std::endl;
     return 1;
 }
